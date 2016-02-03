4be4ada78c3bcdc5aba944e44a19ce65db944ea3
Yves Tiongson <jillmarson@gmail.com>
2016-02-02 22:59:32 -0500

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -4,15 +4,18 @@
 #include <ctype.h>
 #include <string.h>
 
-int main(int argc, char* argv[])
+// My first enciphering program! -Yves T.
+
+int main(int argc, char* argv[]) // char* also string
 {
     int k = atoi(argv[1]);
-    int modk = k % 26;
+    int modk = k % 26; // modulo of keyword
     
     if (argc != 2 || k <= 0)
     {
         printf("Please try a positive number\n");
-        return 1;
+        return 1; // will prompt message if number is equal to or less than zero
+                  // or will return error if no 2nd command line argument was given
     }
     
     else
@@ -24,21 +27,21 @@ int main(int argc, char* argv[])
         for (int i = 0; i < msg[i]; i++)
             if (isupper(msg[i]))
             {
-                int uppercase = msg[i] + modk;
-                    if (uppercase > 90)
+                int uppercase = msg[i] + modk; // for uppercase letters
+                    if (uppercase > 90) // because uppercase Z is 90 in ASCII
                     {
-                        printf("%c", (uppercase - 26));
+                        printf("%c", (uppercase - 26)); // formula to wrap around for uppercase letter
                     }
                     else
                     {
-                        printf("%c", uppercase);
+                        printf("%c", uppercase); // returns uppercase as given by user
                     }
             }
             
             else if (islower(msg[i]))
             {
                 int lowercase = msg[i] + modk;
-                    if (lowercase > 122)
+                    if (lowercase > 122) // because lowercase z is 122 in ASCII
                     {
                         printf("%c", (lowercase - 26));
                     }
